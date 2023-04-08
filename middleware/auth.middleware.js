9d32a79f7a4f633fce4845343cd57ab762203a9a
avinash kumar <avi064448@gmail.com>
2023-04-08 14:01:36 +0530

    second

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -2,8 +2,8 @@ const jwt = require("jsonwebtoken");
 const { AccountModel } = require("../modal/Account.model");
 
 const auth = async(req,res,next)=>{
-    const {email,panNo}= req.body;
-    const account = await AccountModel.find({email,panNo});
+    const {email}= req.body;
+    const account = await AccountModel.find({email});
     if(account.length>0){
         const {_id}= account[0];
         req.body.ID=_id;
